58c0654f27f2081922e1a55a1af99f62f19cd89f
“gabriv-oosgit config --global user.email “lokonki4@gmail.com <lokonki4@gmail.com>
2026-03-12 17:57:03 -0600

    Primer commit: Estructura base de Flet con tabla de productos

--- app/views/mostrar_productos.py
+++ app/views/mostrar_productos.py
@@ -1,40 +1,44 @@
 import flet as ft
-from app.styles.estilos import Buttons, Colors, Inputs, Textos_estilos, Card
+from typing import Any
+from app.services.transacciones_api_productos import list_products,get_product,create_product,update_product,delete_product
+from app.components.popup import show_popup, show_popup_auto_close, show_snackbar, confirm_dialog
+from app.components.error import ApiError, api_error_to_text
+from app.styles.estilos import Colors,Textos_estilos,Card
 
-def productos_view(page: ft.Page):
+def products_view(page:ft.Page) -> ft.Control:
+    rows_data:list[dict[str,Any]]=[]
+    total_items=0
+    total_text = ft.Text("Total de productos: (cargando...)", style=Textos_estilos.H4)
+    
+    columnas=[
+        ft.DataColumn(label=ft.Text("Nombre", style=Textos_estilos.H4)),
+        ft.DataColumn(label=ft.Text("Cantidad", style=Textos_estilos.H4)),
+        ft.DataColumn(label=ft.Text("Ingreso", style=Textos_estilos.H4)),
+        ft.DataColumn(label=ft.Text("Min", style=Textos_estilos.H4)),
+        ft.DataColumn(label=ft.Text("Max", style=Textos_estilos.H4)),
+    ]
 
-    titulo = ft.Text("Aplicación saludo", style=Textos_estilos.H1)
-    subtitulo = ft.Text("Sistema con estilos", style=Textos_estilos.H2)
-
-    nombre = ft.TextField(
-        label="Nombre",
-        **Inputs.INPUT_PRIMARY
+    data=[]
+    data.append(
+        ft.DataRow(
+            cells=[
+                ft.DataCell(ft.Text("nombre1...")),
+                ft.DataCell(ft.Text("cantidad1...")),
+                ft.DataCell(ft.Text("ingreso1...")),
+                ft.DataCell(ft.Text("min1...")),
+                ft.DataCell(ft.Text("max1...")),
+            ]
+        )
     )
 
-    mensaje_text = ft.Text("", style=Textos_estilos.H4)
-
-    def on_click(e):
-        mensaje_text.value = f"Hola {nombre.value}"
-        page.update()
-
-    btn = ft.Button(
-        "Saludar",
-        on_click=on_click,
-        style=Buttons.BUTTON_PRIMARY
+    tabla=ft.DataTable(
+        columns=columnas,
+        rows=data,
+        width=900,
+        heading_row_height=60,
+        heading_row_color=Colors.BG,
+        data_row_max_height=60,
+        data_row_min_height=48
     )
 
-    columna = ft.Column(
-        controls=[titulo, subtitulo, nombre, btn, mensaje_text],
-        spacing=16
-    )
-
-    card = ft.Container(
-        content=columna,
-        **Card.tarjeta
-    )
-
-    return ft.Container(
-        padding=10,
-        alignment=ft.Alignment(0, 0),
-        content=card
-    )
+    return tabla
